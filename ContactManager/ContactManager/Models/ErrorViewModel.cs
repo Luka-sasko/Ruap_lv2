45e3f4c187b6abe843b23424bb47b20cb8ba37c2
Lsasko <148536346+Lsasko@users.noreply.github.com>
2023-11-11 16:18:51 +0100

    TASK1

--- ContactManager/ContactManager/Models/ErrorViewModel.cs
+++ ContactManager/ContactManager/Models/ErrorViewModel.cs
@@ -1,9 +1,8 @@
-namespace ContactManager.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
+namespace ContactManager.Models;
 
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
