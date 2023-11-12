4f539cbe2e8eda021636154ba23597357cdd8a4b
Lsasko <74216417+Luka-sasko@users.noreply.github.com>
2023-11-12 15:45:30 +0100

    Task2

---
+++ ContactManager/ContactManager/Controllers/ContactController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ContactController : ControllerBase
+{
+    [HttpGet]
+    public string[] Get()
+    {
+        return new string[]
+        {
+            "Hello",
+            "World"
+        };
+    }
+}
