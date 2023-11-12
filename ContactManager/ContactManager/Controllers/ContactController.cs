6812d46bda6d04f0c0a984d9025269f37821ad83
Lsasko <74216417+Luka-sasko@users.noreply.github.com>
2023-11-12 16:02:43 +0100

    Task3

--- ContactManager/ContactManager/Controllers/ContactController.cs
+++ ContactManager/ContactManager/Controllers/ContactController.cs
@@ -1,16 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ContactManager.Models; 
 
 [Route("api/[controller]")]
 [ApiController]
 public class ContactController : ControllerBase
 {
     [HttpGet]
-    public string[] Get()
+    public Contact[] Get()
     {
-        return new string[]
+        return new Contact[]
         {
-            "Hello",
-            "World"
+        new Contact
+        {
+            Id = 1,
+            Name = "Glenn Block"
+        },
+        new Contact
+        {
+            Id = 2,
+            Name = "Dan Roth"
+        }
         };
     }
 }
+
